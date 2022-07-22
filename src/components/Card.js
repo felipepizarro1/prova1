94e351e0425f210a33cef28b218226ea62f06a6e
Felipe <fpizarro.ux@gmail.com>
2022-07-22 16:55:06 +0200

    Using useEffect

---
+++ src/components/Card.js
@@ -0,0 +1,34 @@
+
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+
+function Card() {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+      axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(res => {
+        console.log(res)
+        setPosts(res.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
+  return (
+    <div>
+      <ul>
+        {
+          posts.map(item => (
+              <li key={item.id}>
+                {item.title}
+              </li>
+          ))
+        }
+      </ul>
+    </div>
+  )
+}
+
+export default Card
+
